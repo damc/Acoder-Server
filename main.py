0f399202d9f9a5b3918b4682ad2f0aff576bbe4a
Damian Czapiewski <damianczap@outlook.com>
2021-10-05 14:09:58 +0100

    2 Login and registration

--- main.py
+++ main.py
@@ -1,12 +1,114 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, \
+    current_user
+from uuid import uuid4
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SECRET_KEY'] = 'thisissecret'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model):
+    """User model."""
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    api_key = db.Column(db.String(100), unique=True)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Load user by ID."""
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    """Index page."""
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Registration page."""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        accept_terms = request.form['accept_terms']
+
+        if not email or not password or not accept_terms:
+            flash('Please fill all the fields.')
+            return redirect(url_for('register'))
+
+        if accept_terms != 'on':
+            flash('Please accept terms and conditions.')
+            return redirect(url_for('register'))
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(
+                email=email,
+                password=generate_password_hash(password, method='sha256'),
+                api_key=str(uuid4()))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('User with this email already exists.')
+            return redirect(url_for('register'))
+    else:
+        return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Log in page."""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        if not email or not password:
+            flash('Please fill all the fields.')
+            return redirect(url_for('login'))
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            flash('User with this email does not exist.')
+            return redirect(url_for('login'))
+
+        if check_password_hash(existing_user.password, password):
+            login_user(existing_user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Incorrect password.')
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    """Log out page."""
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/dashboard')
+def dashboard():
+    """Dashboard page."""
+    if current_user.is_authenticated:
+        return render_template('dashboard.html', api_key=current_user.api_key)
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
