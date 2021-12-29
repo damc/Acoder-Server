7a0f14aa81030b3cfd4382c226ed124de98e3a16
Damian Czapiewski <damianczap@outlook.com>
2021-12-29 12:36:40 +0000

    30 Improve extract code prompt and small refactor

--- solver/question.py
+++ solver/question.py
@@ -5,4 +5,11 @@ from .codex import codex, prompt
 
 def answer(question: str, user_id: Optional[str] = None) -> str:
     prompt_ = prompt("question.txt", question=question)
-    return codex(prompt_, temperature=0.1, stop='"""', user=user_id).strip()
+    answer_ = codex(
+        prompt_,
+        max_tokens=500,
+        temperature=0.1,
+        stop='"""',
+        user=user_id
+    )
+    return answer_.strip()
