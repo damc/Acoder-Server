72bc03d91c7e65c4c2f25e86d913db0c2e05db4b
damc <damiancz@mailfence.com>
2023-07-12 08:33:04 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export FLASK_APP=server
 export FLASK_ENV=development
-export DATABASE_URI='mysql://root:dijkstr4@localhost/acoder'
+export DATABASE_URI='mysql://root:password@localhost/acoder'
 export REDIS_URL='redis://localhost:6379/0'
 export SECRET_KEY='ilovejustinbieber'
 export CODEX_PLUS_PLUS_BASE_URL='http://127.0.0.1:5000'
